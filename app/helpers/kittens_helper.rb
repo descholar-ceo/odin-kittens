0d91d65791279ca165ec27e4f6d2f27b9f9b5a9c
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-27 20:54:37 +0200

    Add newkitten link on index

--- app/helpers/kittens_helper.rb
+++ app/helpers/kittens_helper.rb
@@ -1,12 +1,12 @@
 module KittensHelper
   def display_all_kittens(kittens)
-    result = ''
+    result = "<div>#{link_to('Create a new kitten', new_kitten_path)}</div>"
     if kittens.count.positive?
       kittens.each do |kitten|
         result << "<br/><div>#{link_to(kitten.name, kitten)}</div>"
       end
     else
-      result << '<span>No kitten found yet sorry!</span>'
+      result << '<br/><span>No kitten found yet sorry!</span>'
     end
     result.html_safe
   end
