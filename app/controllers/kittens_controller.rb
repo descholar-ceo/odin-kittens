945e0dcd5695578009b91769eba276c8188bdbdc
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-27 21:30:24 +0200

    Add json format

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,10 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+    respond_to do |format|
+      format.html { render 'index.html.erb' }
+      format.json { render :json => @kittens }
+    end
   end
 
   def show
