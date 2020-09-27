e4f76638f04cf1d2134d3926d61a3c75d6b7f364
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-27 19:38:36 +0200

    Fix rubocop lin error

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,23 +1,23 @@
 class KittensController < ApplicationController
-    def index
-        @kittens = Kitten.all
-    end
+  def index
+    @kittens = Kitten.all
+  end
 
-    def show
-        @kitten = Kitten.find(param[:id])
-    end
+  def show
+    @kitten = Kitten.find(param[:id])
+  end
 
-    def new
-        @kitten = Kitten.new
-    end
+  def new
+    @kitten = Kitten.new
+  end
 
-    def edit
-        @kitten = Kitten.new(kitten_params)
-    end
+  def edit
+    @kitten = Kitten.new(kitten_params)
+  end
 
-    private
-    
-    def kitten_params
-        params.require(:kitten).permit(:name, :age, :softness, :cuteness)
-    end
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+  end
 end
