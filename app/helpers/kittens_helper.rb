63c65a7502508a951fd751d9845aefdae875d449
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-27 20:10:14 +0200

    Fix rubocop lint error

--- app/helpers/kittens_helper.rb
+++ app/helpers/kittens_helper.rb
@@ -1,6 +1,13 @@
 module KittensHelper
   def display_all_kittens(kittens)
-    kittens.each do |kitten|
+    result = ''
+    if kittens.count.positive?
+      kittens.each do |kitten|
+        result << link_to(kitten.name, kitten)
+      end
+    else
+      result << '<span>No kitten found yet sorry!</span>'
     end
+    result.html_safe
   end
 end
